43bb5f97406374abd0a2927ba5f1414ada832b27
MARIA ESTEPHANI <mariaestephaniso@gmail.com>
2026-09-18 15:26:20 -0300

    Change modal display style from block to flex

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function abrirModalProjeto() {
     let modal = document.getElementById("modal-projeto");
     if (modal) {
-        modal.style.display = "block";
+        modal.style.display = "flex";
     }
 }
 
